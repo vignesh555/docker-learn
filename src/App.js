ead94a6c969108b350b507d01d14336c9a7c1ddd
vignesh <vigneshdotnet@gmail.com>
2023-06-03 12:49:26 +0800

    github

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React new soon
+          Learn React new soon123
         </a>
       </header>
     </div>
